0851092f363a9186eaf9124397329c0ef68db211
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2017-11-27 13:24:25 -0600

    Add files via upload

---
+++ robotturnon/13.1.2.py
@@ -0,0 +1,13 @@
+import time
+import setup
+from setup import RPL
+import RoboPiLib as RPL
+RPL.servoWrite(0,1000)
+start = time.time()
+
+elapsed = (time.time() - start)
+for integer in time.time():
+    if elapsed = 3:
+        import RoboPiLib as RPL
+        import setup
+        RPL.servoWrite(0,0)
