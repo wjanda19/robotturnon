523d85ee4925a4e272b5a08250b065d7ffadb345
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2017-11-29 13:45:49 -0600

    Update 13.1.2.py

--- robotturnon/13.1.2.py
+++ robotturnon/13.1.2.py
@@ -5,18 +5,21 @@ import RoboPiLib as RPL
 RPL.servoWrite(0,1000)
 start = time.time()
 
-global x
 x = 2
+global x
+elapsed = (time.time() - start)
+global elapsed
 
 while True:
     global x
     x = x % 2
-    elapsed = (time.time() - start)
     elapsed = int(elapsed)
-    if x == 0:
-        RPL.servoWrite(0,1000)
-        x = x + 1
-    if x == 1:
-        RPL.servoWrite(0,0)
-        x = x - 1
+    if elapsed == 3:
+       if x == 0:
+          RPL.servoWrite(0,1000)
+          x = x + 1
+    if elapsed == 6:
+       if x == 1:
+          RPL.servoWrite(0,0)
+          x = x - 1
 
