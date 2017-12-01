f14c4fe0348c2f26fdd0000bdad066cc0eef713b
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2017-12-01 13:32:30 -0600

    Update 13.2.1.py

--- robotturnon/13.2.1.py
+++ robotturnon/13.2.1.py
@@ -12,7 +12,7 @@ while True:
   if RPL.digitalRead(sensor_pin) == 1:
      import RoboPiLib as RPL
      import setup
-     RPL.servoWrite(-1000,-1000)
+     RPL.servoWrite(1000,1000)
   if RPL.digitalRead(sensor_pin) == 0:
      import RoboPiLib as RPL
      import setup
