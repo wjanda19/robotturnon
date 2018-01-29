606ddcde93e741caafd0361ea7b18f4540e2a743
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2018-01-29 13:34:04 -0600

    Update 13.2.3.py

--- robotturnon/13.2.3.py
+++ robotturnon/13.2.3.py
@@ -2,15 +2,21 @@ import setup
 from setup import RPL
 import RoboPiLib as RoboPi
 
+RoboPi.RoboPiInit(“/dev/ttyAMA0”,115200)
+
+RoboPi.pinMode(1,RoboPi.OUTPUT)
+RoboPi.digitalWrite(16,1)
+print RoboPi.analogRead(0)
+
 #RPL.servoWrite(0,1000)
 #RPL.servoWrite(1,1000)
 
-x = 1
-while x == 1:
-  RoboPi.analogRead(1)
-  AnalogRead = RoboPi.analogRead(1)
-  AnalogRead = int(AnalogRead)
-  print AnalogRead
+#x = 1
+#while x == 1:
+ # RoboPi.analogRead(1)
+ # AnalogRead = RoboPi.analogRead(1)
+ # AnalogRead = int(AnalogRead)
+ # print AnalogRead
   
   
  # Dist = (500 * AnalogRead)/1024
