00a421ef887386da45fce4900d48dd16927982cb
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2018-01-29 13:31:36 -0600

    Update 13.2.3.py

--- robotturnon/13.2.3.py
+++ robotturnon/13.2.3.py
@@ -2,8 +2,8 @@ import setup
 from setup import RPL
 import RoboPiLib as RoboPi
 
-RPL.servoWrite(0,1000)
-RPL.servoWrite(1,1000)
+#RPL.servoWrite(0,1000)
+#RPL.servoWrite(1,1000)
 
 x = 1
 while x == 1:
@@ -11,7 +11,9 @@ while x == 1:
   AnalogRead = RoboPi.analogRead(1)
   AnalogRead = int(AnalogRead)
   print AnalogRead
-  Dist = (500 * AnalogRead)/1024
-  print Dist
+  
+  
+ # Dist = (500 * AnalogRead)/1024
+#  print Dist
   
 #import RoboPiLib as RoboPi
