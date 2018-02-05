b3e662da75df4fab33ffdb82f97dee086eb0fd4f
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2018-02-05 13:47:39 -0600

    Update 13.2.4.py

--- robotturnon/13.2.4.py
+++ robotturnon/13.2.4.py
@@ -31,9 +31,7 @@ while True:
       while True:
         elapsed = (time.time() - start)
         x = 1
-        
-        print int(elapsed)
-        if elapsed % 3 == 0:
+        if int(elapsed) % 3 == 0:
           RPL.servoWrite(0,1000)
           break
      
