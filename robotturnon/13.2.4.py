e86667e9e56a8883e64633818ccf81a581fa21c0
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2018-02-05 13:22:43 -0600

    Update 13.2.4.py

--- robotturnon/13.2.4.py
+++ robotturnon/13.2.4.py
@@ -4,9 +4,23 @@ import post_to_web as PTW # see post_to_web.py for instructions
 
 sensor_pin = 16
 RPL.pinMode(sensor_pin,RPL.INPUT)
+x = 1
 
 while True:
-
+  if x == 1:
+     import time
+     import setup
+     from setup import RPL
+     import RoboPiLib as RPL
+     start = time.time()
+     start = int(start)
+     if start == 3:
+        import RoboPiLib as RPL
+        import setup
+        RPL.servoWrite(0,0)
+        RPL.servoWrite(1,0)
+        exit
+      
   if RPL.digitalRead(sensor_pin) == 1:
      import RoboPiLib as RPL
      import setup
@@ -18,13 +32,9 @@ while True:
      import setup
      RPL.servoWrite(0,1450)
      RPL.servoWrite(1,1550)
+     x = 1
      
-     import time
-     import setup
-     from setup import RPL
-     import RoboPiLib as RPL
-     start = time.time()
-        
+      
      while True:
         elapsed = (time.time() - start)
         elapsed = int(elapsed)
